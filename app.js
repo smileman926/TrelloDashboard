834da962f7337b6112c2450eb6ea808f8bf30c40
unknown <loyalsun94415@hotmail.com>
2020-01-29 11:42:12 +0800

    test check

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ mongoose.set('debug', true);
 //   });
 // });
 if (process.env.NODE_ENV === 'production') {
-    app.get('/', (req, res) => {
+    app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname,'client','build', 'index.html'));
     });
   // const path = require('path')
